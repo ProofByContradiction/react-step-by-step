208ff5e36c259242ef4ea016cb999a17798a1bdf
ProofByContradiction <Mckeema@gmail.com>
2022-06-02 11:35:01 -0400

    Playing with React elements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const user = {
+  firstName: 'Matthew',
+  lastName: 'McKee',
+  avatarUrl: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//These are identical. All three objects are "React elements".
+const greeting1 = (<h1 className='greeting'>Hello, world!</h1>);
+const greeting2 = React.createElement('h1',{className: 'greeting'},'Hello, world!');
+const greeting3 = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello, world!'
+  }
+};
+
+const avatar = <img src ={user.avatarUrl}/>
+const link = <a href='https://www.reactjs.org'>link</a>
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <div>
+      <h1>Hello, {formatName(user)}!</h1>
+      <h2>Good to see you here.</h2>
+    </div>;
+  }
+  return <div>
+    <h1>Hello, Stranger.</h1>
+    <h2>Good to see you here.</h2>
+  </div>;
+}
